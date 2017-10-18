b8d0c2f9b2fbc164fd59a44b92fc72156516df68
Jacob <jakegod14@gmail.com>
2017-10-18 12:15:54 -0600

    Init commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Adinkra from "components/Adinkra"
+class App extends React.Component {
+  render() {
+    return (
+      <Adinkra name="asase-ye-duru"/>
+    )
+  }
+}
+export default App
